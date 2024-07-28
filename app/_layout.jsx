8ec309e0d0f311999956652e31fb054e9c7063ae
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2024-07-28 19:21:35 +0300

    Add protected stack

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -56,6 +56,12 @@ export default function RootLayout() {
           headerShown: false,
         }}
       />
+      <Stack.Screen
+        name="(protected)"
+        options={{
+          headerShown: false,
+        }}
+      />
     </Stack>
   );
 }
